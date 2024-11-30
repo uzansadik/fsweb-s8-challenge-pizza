5de2aebe5b023cccdf5dc7723477af36ccd84184
uzansadik <uzansadik@gmail.com>
2024-11-30 06:35:05 +0300

    update app comp.

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,13 @@ import Home from './Pages/Home';
 import './App.css';
 
 function App() {
-  return <h1>Order pizza</h1>;
+  return (
+    <div>
+      <h1>Headr</h1>
+      <h1>Breadcrump</h1>
+      <h1>Diğer</h1>
+    </div>
+  );
 }
 
 export default App;
